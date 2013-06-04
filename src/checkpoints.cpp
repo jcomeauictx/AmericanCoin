42e9955e4254bd6be9039919cfa8edf69732b25d
AMCcoin <john@AMCcoin.com>
2013-06-04 09:31:50 -0500

    Added checkpoint

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -29,7 +29,8 @@ namespace Checkpoints
             (     0, uint256("0xe235425c8314df2f26b94d89fd5582edc74d80cf39d105107ea6cd12d8898491"))
             (     9000, uint256("0x6aaddca66be07c4df9c5bb14ec5a46edfa13b0e208187b1275744e4354339a8b"))			
             (     12150, uint256("0x91f24da86e5aaf6cfcf146e62d6b6cdc63e1b1ce699f776399e311d707cc1aa6"))		
-            (     13550, uint256("0x0c50d43e4982187e704c8c9d2b33d2617aed69ee471dffb744e58414c961cb11"))
+            (     13550, uint256("0x0c50d43e4982187e704c8c9d2b33d2617aed69ee471dffb744e58414c961cb11"))	
+            (     15121, uint256("0x040e045718181b2467df5c2eb2cd3a95ee45240b54cbffc68ef00aa254a961a8"))
 			
 
             ;
